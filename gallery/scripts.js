a9a406865e5ba29b2f33ebdd56d40d1fa297d2c3
nyhp2020 <nyhp@freemail.hu>
2020-04-24 18:07:36 +0200

    Upload 0424
    Gallery developed.

--- gallery/scripts.js
+++ gallery/scripts.js
@@ -1,64 +1,66 @@
-/* let data = {
-    photo: 'images/photo1.jpg',
-    title: 'My title',
-    description: 'What happened here, why is this a very nice image'
-}; */
 
 let currentPhoto = 0;
 
 let imagesData = [{
-        photo: 'images/P1010038.jpg',
-        title: 'Bled 1',
-        description: 'At lake Bled'
-    },
-    {
-        photo: 'images/P1010043.jpg',
-        title: 'Bled 2',
-        description: 'At lake Bled'
-    },
-    {
-        photo: 'images/P1010051.jpg',
-        title: 'Bled 3',
-        description: 'At lake Bled'
-    },
-    {
-        photo: 'images/P1010054.jpg',
-        title: 'Bled 4',
-        description: 'At lake Bled'
-    },
-    {
-        photo: 'images/P1010055.jpg',
-        title: 'Bled 5',
-        description: 'At lake Bled'
-    },
-    {
-        photo: 'images/P1010098.jpg',
-        title: 'Malta Hochalmstraße',
-        description: 'Kölnbrein barrier,Airwalk viewpoint'
-    },
-    {
-        photo: 'images/P1010101.jpg',
-        title: 'Malta Hochalmstraße',
-        description: 'Reservoir behind the Kölnbrein barrier'
-    }
+    photo: 'images/P1010038.jpg',
+    title: 'Bled 1',
+    description: 'At lake Bled.'
+},
+{
+    photo: 'images/P1010043.jpg',
+    title: 'Bled 2',
+    description: 'At lake Bled.'
+},
+{
+    photo: 'images/P1010051.jpg',
+    title: 'Bled 3',
+    description: 'At lake Bled.'
+},
+{
+    photo: 'images/P1010054.jpg',
+    title: 'Bled 4',
+    description: 'Bled castle foreground of mountains.'
+},
+{
+    photo: 'images/P1010055.jpg',
+    title: 'Bled 5',
+    description: 'At lake Bled.'
+},
+{
+    photo: 'images/P1010098.jpg',
+    title: 'Malta Hochalmstraße',
+    description: 'Kölnbrein barrier,Airwalk viewpoint.'
+},
+{
+    photo: 'images/P1010101.jpg',
+    title: 'Malta Hochalmstraße',
+    description: 'Reservoir behind the Kölnbrein barrier.'
+}
 ];
+
+//load thumbnails
+imagesData.forEach((item, index) => {
+    $('.thumbnail-container')
+        .append(`<div class="thumbnail" data-index="
+        ${index}"><div class="title">${item.title}</div><img src="${item.photo}" ></div>`);
+});
 
 function loadPicture(num) {
     $('#photo').attr('src', imagesData[num].photo);
     $("#photo-title").text(imagesData[num].title);
     $("#photo-description").text(imagesData[num].description);
+    //selectThumbnail
+    $("div.thumbnail").css("border-color", "grey");
+    $("div.thumbnail").eq(num).css("border-color", "#de6f4e");
 }
 
 $('#photo').attr('width', "100%");
 $('#photo').attr('height', "100%");
 
-/* $('#photo').attr('src', imagesData[currentPhoto].photo);
-$("#photo-title").text(imagesData[currentPhoto].title);
-$("#photo-description").text(imagesData[currentPhoto].description); */
-
 loadPicture(currentPhoto);
 
-$("#lb").on('click', function() {
+//left button
+$("#lb").on('click', function () {
     if (currentPhoto > 0) {
         currentPhoto--;
     } else {
@@ -67,7 +69,8 @@ $("#lb").on('click', function() {
     loadPicture(currentPhoto);
 });
 
-$("#rb").on('click', function() {
+//right button
+$("#rb").on('click', function () {
     if (currentPhoto < imagesData.length - 1) {
         currentPhoto++;
     } else {
@@ -75,3 +78,22 @@ $("#rb").on('click', function() {
     }
     loadPicture(currentPhoto);
 });
+
+//thumbnails
+$('.thumbnail-container').click((event) => {
+    loadPicture(currentPhoto = parseInt($(event.target)
+        .parent()
+        .attr("data-index")));
+});
+
+/* imagesData.forEach((item, index) => {
+    alert($('.thumbnail-container')
+    .append(`<div class="thumbnail" data-index="${index}"><img src="${item.photo}" ></div>`)
+    .children("div:last-child") //last appended child of ".thumbnail-container" !
+    .attr("data-index"));
+}); */
+
+
+/* $('.thumbnail').click((event) => {
+    loadPicture(currentPhoto = parseInt($(event.target).parent().attr("data-index")));
+}); */
